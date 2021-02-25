5566d54e2a298f83dab8c52e47b7e988f94a984a
suochan <suochantsao@gmail.com>
2021-02-25 17:48:35 +0800

    將價格render在網頁上

--- js/info.js
+++ js/info.js
@@ -5,6 +5,8 @@ const config         = { headers: { Authorization: `Bearer ${token}` } };
 
 // DOM
 const picturesArea = document.querySelector('.pictureBlock'); 
+const countBlock   = document.querySelector('.countArea');
+const totalPrice   = document.querySelector('.priceArea');
 const infoBlock    = document.querySelector('.roomInfoBlock');
 const checkInBlock = document.querySelector('.checkInBlock');
 const amenityBlock = document.querySelector('.amenitiesBlock');
@@ -17,13 +19,14 @@ const phoneInput   = document.getElementById('phoneNumInput');
 
 // Object
 let roomInfo;
+let selectDates = [];
 
 // Check the url's id
 let currentId = window.location.search.slice(4);
 
 // ,.♫_____________________♪____________________♫,. //
 
-// GET specific room information
+// *GET specific room information
 axios
   .get(roomDetailUrl+currentId,config)
   .then(res => {
@@ -146,7 +149,7 @@ function loaderRender(){
 window.addEventListener('load',loaderRender);
 
 // ,.♫_____________________♪____________________♫,. //
-// Calendar Block
+// *Calendar Block
 flatpickr('#calendar-zhtw', {
     "locale": "zh_tw",
     "dateFormat": "Y/m/d",
@@ -155,7 +158,7 @@ flatpickr('#calendar-zhtw', {
 });
 
 // ,.♫_____________________♪____________________♫,. //
-// Dialog Block
+// *Dialog Block
 function dialogRender(){
     dialogBlock.className += ' dialogStyle';
     dialogBlock.classList.remove('dialogNone');
@@ -181,21 +184,109 @@ dialogBlock.addEventListener('click',cancleReserve);
 
 // Calendar in Dialog 
 const calendar = flatpickr('#startDate', {
-    "minDate": "today",
+    "minDate": new Date().fp_incr(1),
     "dateFormat": "Y/m/d",
     "plugins": [new rangePlugin({ input: "#endDate"})]
 });
 
-// Testing to get the value from each input
-window.addEventListener('mouseup',()=>console.log(calendar.selectedDates));
-
-confirmBtn.addEventListener('click',()=>console.log(nameInput.value));
-confirmBtn.addEventListener('click',()=>console.log(phoneInput.value));
-
-// confirmBtn.addEventListener('click',test);
-function test(e){
-    console.log(e);
+// Get the dates and render price of the room
+function dateCalculate(){
+    let selectAry   = calendar.selectedDates;
+    selectDates = DateList(selectAry[0],selectAry[1]);
+    priceRender();
 }
 
+// Get the date list
+function DateList(start,end){
+    if(start > end) {
+        [start, end] = [end, start]
+        // 如果是選擇月底跨到隔月初的話，選擇的startdate會大於enddate，因此要交換這兩個陣列的值
+    }
+    let startDate  = new Date(start);
+    let endDate    = new Date(end);
+    // 將傳進來的Object再次轉換成日期的資料格式，因此使用new關鍵字去建立該值
+
+    let totalDates = (endDate - startDate) / (1000 * 60 * 60 * 24); 
+    // 1毫秒=1/1000秒，所以先將相減後的日期毫秒差再轉換成天數
+    totalDates     = Math.ceil(totalDates);
+    // 使用math的無條件進位，讓計算天數的結果直接進位
+        
+    let list =[];
+    for(let index = 0; index <= totalDates; index++){
+        let day = new Date(start);
+        // 這邊一定要使用new關鍵字來建立新的日期然後讓陣列來操作
+        day.setDate(day.getDate()+ index);
+        // 設定日期(開始日期+index)
+        // setDate() -> 設定Date是幾日？設定1的話，日期則為X年X月1日
+        // getDate() -> 取得Date是幾日？取得start的dates，取得開始日之後就依照上面的for迴圈去+1
+
+        list.push(day.toString());
+        // 因為要使用字串的資料，所以使用toString()之後再push
+    }
+    return list;
+     
+}
+
+// Render room price in the countArea
+function priceRender(){
+    let dayNum       = 0;
+    let weekendNum   = 0;
+    let totalFee     = 0;
+    let dayPrice     = roomInfo.normalDayPrice;
+    let weekendPrice = roomInfo.holidayPrice;
+
+    // 抓取星期然後再去判定是否為假日？
+    selectDates.forEach((item) => {
+        // 取得每一個日期為星期幾？
+        let weekName   = item.slice(0,3);
+
+        // 判斷選擇的該日期是否為假日？
+        if(weekName === 'Fri' || weekName === 'Sat'){
+            weekendNum+=1;
+            totalFee+=weekendPrice;
+        }else{
+            dayNum+=1;
+            totalFee+=dayPrice;
+        }
+        
+    });
+
+    let str = `<li class="normalDays">平日時段</li>
+                <li class="normalDays daysCount">${dayNum}夜</li>
+                <li class="holidays">假日時段</li>
+                <li class="holidays daysCount">${weekendNum}夜</li>`;
+    countBlock.innerHTML = str;
+
+    let priceStr = `= <span>NT.${totalFee}</span>`;
+    totalPrice.innerHTML = priceStr;
+}
+
+dialogBlock.addEventListener('mouseout',dateCalculate);
+
+// Confirm to order the rooms
 const fp = document.querySelector("#startDate")._flatpickr;
-confirmBtn.addEventListener('click',()=>console.log(fp));
+
+confirmBtn.addEventListener('click',confirmInfo);
+
+function confirmInfo(){
+    let nameInfo  = nameInput.value;
+    let phoneInfo = phoneInput.value;
+    let currentYear;
+    let currentMonth;
+    let currentDate;
+
+    selectDates.forEach((item)=>{
+        console.log(item);
+        currentYear  = item.slice(11,15);   
+        // currentMonth = item.slice(11,15);
+        // currentDate  = item.slice(11,15);
+        
+        // console.log(`${currentYear}-${currentMonth}-${currentDate}`);
+    });
+
+    // console.log(currentYear);
+
+    // console.log(nameInfo);
+    // console.log(phoneInfo);
+    // console.log(selectDates);
+}
